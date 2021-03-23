324e69c2f7564baebef0435d1f0ff76ecfaec297
rayyan <rayyan10@bellsouth.net>
2021-03-23 13:37:10 -0500

    💅  STYLE: fix more styling; ticker

--- clientside/src/components/DispoHeader.jsx
+++ clientside/src/components/DispoHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Alert } from "react-bootstrap";
+import { Alert, Container } from "react-bootstrap";
+import { Leader } from './Text';
 
 export default function DispoHeader() {
 
@@ -7,14 +8,23 @@ export default function DispoHeader() {
   let updates = ["Hello World", "somebody has just joined", "Pineapple"];
 
   return (
-    <Alert variant="info">
-      <div data-role="marquee">
-        {updates.map((update, i) =>
-          <div key={i}>
-            {update}
-          </div>)}
-        </div>
-      </Alert>
+    <div>
+      <Container className="d-flex flex-row align-items-center">
+        <h3 className="text-muted fw-lighter">{"Disposocial (info)"}</h3>
+      <Container className="w-50 bg-primary">
+        <Container className="bg-light px-0 py-3 h-100">
+          <div
+            id="ticker"
+            data-role="marquee">
+            {updates.map((update, i) =>
+              <div key={i}>
+                {update}
+              </div>)}
+            </div>
+          </Container>
+        </Container>
+      </Container>
+      </div>
     );
 
   }
