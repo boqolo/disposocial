4b28f42c648647b2a2f2dd4a803491726e9078b3
rayyan <rayyan10@bellsouth.net>
2021-03-29 04:24:56 -0500

    implement dispo dicovery pt 2 (haversine algo)

--- serverside/lib/disposocial/dispos.ex
+++ serverside/lib/disposocial/dispos.ex
@@ -3,6 +3,11 @@ defmodule Disposocial.Dispos do
   The Dispos context.
   """
 
+  @radius_of_earth 3_959 # in miles (converted from 6_371 km)
+  @dispo_radius 5 # in miles
+  @lat_factor 0.006910164958 # see docs for `get_all_near` below
+  @lng_factor 0.03368556573
+
   import Ecto.Query, warn: false
   alias Disposocial.Repo
 
@@ -36,6 +41,81 @@ defmodule Disposocial.Dispos do
 
   """
   def get_dispo!(id), do: Repo.get!(Dispo, id)
+
+  defp rad(deg) do
+    deg * (:math.pi() / 180)
+  end
+
+  @doc """
+  Calculates the Haversine distance between two points on Earth in feet.
+  The Haversine distance is the distance between two points on a sphere.
+  I use the accepted average radius of Earth (6,371 km) as the basis
+  of conversion to US Customary feet.
+
+  Adapted from a Python variation here:
+
+  https://community.esri.com/t5/coordinate-reference-systems/distance-on-a-sphere-the-haversine-formula/ba-p/902128
+  """
+  def haversine_dist({lat1, lng1}, {lat2, lng2}) do
+    phi_1 = rad(lat1)
+    phi_2 = rad(lat2)
+    delta_phi = rad(lat2 - lat1)
+    delta_lam = rad(lng2 - lng1)
+    a = :math.pow(:math.sin(delta_phi / 2), 2) + :math.cos(phi_1) * :math.cos(phi_2) * :math.pow(:math.sin(delta_lam / 2), 2)
+    c = 2 * :math.atan2(:math.sqrt(a), :math.sqrt(1 - a))
+    @radius_of_earth * c
+  end
+
+  def haversine_dist_mi({lat1, lng1}, {lat2, lng2}) do
+    haversine_dist({lat1, lng1}, {lat2, lng2}) / 5_280
+  end
+
+  @doc"""
+  Gets the Dispos with coordinates within a radius of @dispo_radius.
+  Uses Haversine distance calculation formula.
+
+  NOTE: the Ecto query api (to my knowledge) will not allow conditionally
+  selecting based on the haversine distance function so an initial query
+  for nearby dispos is done to get plausible candidates before filtering
+  for Dispos within the haversine distance.
+
+  Here are two example geographic coordinates. IRL, the distance between them
+  (in a direct line) is ~76.63 miles (hypotenuse). Using Driver, AR
+  as a horizontal point (~67.29 miles West), I derive a latitude constant (L_x)
+  which is multiplied by the @dispo_radius to give a rough latitudinal mile
+  radius. The longitudinal constant (L_y) is derived similarly after
+  calculating the delta in miles for the final (vertical) side using the
+  Pythagorean theorem.
+
+  Memphis: (35.149532, -90.048981)
+  Jackson: (35.614517, -88.813950)
+
+  L_x ~= 0.006910164958
+  L_y ~= 0.03368556573
+
+  As a safe bet, the initial query selects Dispos with latitudes of
+  delta_lat_max = @lat_factor * @dispo_radius and longitudes of
+  delta_lng_max = @lng_factor * @dispo_radius
+
+  All calculations here use miles with coordinate degrees.
+  """
+  def get_all_near(qlat, qlng) do
+    # TODO lat and lng checking. fix this later
+    query = cond do
+      qlat > 0 && qlng > 0 -> from(d in Dispo, where: d.latitude > 0 and d.longitude > 0)
+      qlat > 0 && qlng < 0 -> from(d in Dispo, where: d.latitude > 0 and d.longitude < 0)
+      qlat < 0 && qlng > 0 -> from(d in Dispo, where: d.latitude < 0 and d.longitude > 0)
+      qlat < 0 && qlng < 0 -> from(d in Dispo, where: d.latitude < 0 and d.longitude < 0)
+      true -> nil
+    end
+
+    in_radius = fn(dispo) ->
+      haversine_dist_mi({qlat, qlng}, {dispo.latitude, dispo.longitude}) <= @dispo_radius end
+
+    query
+    |> Repo.all()
+    |> Enum.filter(in_radius)
+  end
 
   @doc """
   Creates a dispo.
