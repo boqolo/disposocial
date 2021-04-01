5fd155edcaff4a7f8672a0ec2b3e5ae750857410
rayyan <rayyan10@bellsouth.net>
2021-03-31 20:20:18 -0500

    serverside config

--- serverside/config/prod.secret.exs
+++ serverside/config/prod.secret.exs
@@ -35,7 +35,7 @@ config :disposocial, DisposocialWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :disposocial, DisposocialWeb.Endpoint, server: true
+config :disposocial, DisposocialWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
