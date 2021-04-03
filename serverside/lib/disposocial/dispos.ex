e5db76b158e890c3632f165cfd11e4fe95a9fc5e
rayyan <rayyan10@bellsouth.net>
2021-04-03 02:11:36 -0500

    fix dispo duration float (cont)

--- serverside/lib/disposocial/dispos.ex
+++ serverside/lib/disposocial/dispos.ex
@@ -160,7 +160,9 @@ defmodule Disposocial.Dispos do
   def create_dispo(attrs) do
     Logger.debug("Processing Dispo args")
     deathDate = unless is_nil(attrs["duration"]) do
-      added_time = Float.round(String.to_float(attrs["duration"])) * 3_600 # in seconds. duration received as hours
+      added_time =
+        Float.round(String.to_float(attrs["duration"]), 2) * 3_600 # in seconds. duration received as hours
+        |> trunc()
       DateTime.utc_now()
       |> DateTime.add(added_time, :second, Tzdata.TimeZoneDatabase)
     end
