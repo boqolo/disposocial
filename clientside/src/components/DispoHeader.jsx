39a6b57873b364b7280282d40b7825c046b48ca8
rayyan <rayyan10@bellsouth.net>
2021-04-08 19:59:35 -0500

    add info msg toasts

--- clientside/src/components/DispoHeader.jsx
+++ clientside/src/components/DispoHeader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Alert, Container, Row, Col, Button, Navbar, Nav  } from "react-bootstrap";
+import { Alert, Container, Row, Col, Button, Navbar, Nav, Toast  } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { Leader } from './Text';
 import { ch_leave_dispo } from '../socket';
 import { remove_at, reset_dispo_state, clear_errors } from '../util';
+import store from '../store';
 import HeaderAlert from './Alert';
 
 function DispoHeader({info, success, error, ticker, dispatch}) {
@@ -42,6 +43,28 @@ function DispoHeader({info, success, error, ticker, dispatch}) {
           </Navbar.Collapse>
         </Navbar>
       </Container>
+      {info.length > 0 && info.map((msg, i) =>
+        <Toast
+          key={`info-${i}`}
+          autohide
+          delay={5000}
+          onClose={() => {
+            store.dispatch({ type: "info/set", data: remove_at(info, i) })
+          }}
+          className="notification">
+          <Toast.Header closeButton={false}>
+            <Col><strong>Info</strong></Col>
+            <Col xs="auto">
+              <Button
+                size="sm"
+                className="border-0 btn-close"
+                onClick={() => {
+                  store.dispatch({ type: "info/set", data: remove_at(info, i) })
+                }}></Button>
+            </Col>
+          </Toast.Header>
+          <Toast.Body>{msg}</Toast.Body>
+        </Toast>)}
       <Col className="mt-1 mx-auto w-75">
         {success.length > 0 && success.map((msg, i) =>
           <HeaderAlert key={`succ-${i}`} i={i} msg={msg} success={success} group="success" />)}
