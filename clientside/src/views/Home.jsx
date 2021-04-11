1abf49ec72d0d5f5500da82666f22414875473f8
rayyan <rayyan10@bellsouth.net>
2021-04-10 23:32:17 -0500

    edit home page wording

--- clientside/src/views/Home.jsx
+++ clientside/src/views/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Col xs="10" md="9" className="mx-auto">
         <Jumbotron className="rounded my-3 mx-auto">
           <Leader>{"Pop up. Tear down."}</Leader>
-          <h2><small>{"Disposocial is an experiment. It is a localized social media bulletin that self-destructs after an amount of time."}</small></h2>
+          <h2><small>{"Disposocial is a localized social media board that self-destructs after an amount of time."}</small></h2>
           <p>{"There is no one network. Interact with others by creating or joining Dispos within your geographic radius."}</p>
           <p>{"Attention spans are short and there's already enough content out there on the web."}</p>
           <p>{"Dispos are fleeting. They're here, and they're gone."}</p>
